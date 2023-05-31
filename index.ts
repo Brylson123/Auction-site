08d81862ec57e94427cca9eef02356e6dc4b753d
bryla <csowscy@gmail.com>
2023-05-31 14:28:25 +0200

    added eslint

--- index.ts
+++ index.ts
@@ -1,19 +1,17 @@
-import * as express from "express";
-import * as methodOverride from "method-override";
-import {urlencoded} from "express";
-import './utils/db'
+import * as express from 'express';
+import * as methodOverride from 'method-override';
+import { urlencoded } from 'express';
+import './utils/db';
 
 const app = express();
 
-app.use(methodOverride("_method"))
+app.use(methodOverride('_method'));
 app.use(urlencoded({
-    extended: true,
-}))
+  extended: true,
+}));
 
 app.get('/', (req, res) => {
-    res.send('It works!')
-})
+  res.send('It works!');
+});
 
-app.listen(3000, 'localhost',()=>
-    console.log('listening on http://localhost:3000'
-))
+app.listen(3000, 'localhost', () => console.log('listening on http://localhost:3000'));
--- utils/db.ts
+++ utils/db.ts
@@ -1,10 +1,9 @@
-import {createPool} from "mysql2/promise";
-
+import { createPool } from 'mysql2/promise';
 
 export const pool = createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'auction',
-    namedPlaceholders: true,
-    decimalNumbers: true,
-})
+  host: 'localhost',
+  user: 'root',
+  database: 'auction',
+  namedPlaceholders: true,
+  decimalNumbers: true,
+});
