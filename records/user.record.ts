d24fb3cbf1ed199904a3b0c94fa143ae77be296a
bryla <csowscy@gmail.com>
2023-06-01 17:17:01 +0200

    change userrecord

--- records/user.record.ts
+++ records/user.record.ts
@@ -1,28 +1,32 @@
 import {v4 as uuid} from "uuid";
 import {pool} from "../utils/db";
 
-export class UserRecord{
+
+export class UserRecord {
     public id?: string;
     public readonly name: string;
     public readonly email: string;
-    public password: string;
+    public readonly password: string;
 
-    constructor(obj: UserRecord) {
+    constructor(obj: Omit<UserRecord, 'insert'>) {
         const {id, name, email, password} = obj;
+
 
         this.id = id ?? uuid();
         this.name = name;
         this.email = email;
         this.password = password;
+
     }
 
-    async insert(): Promise<string>{
-        await pool.execute("INSERT INTO `users`(id, name, email, password) VALUES (:id, :name, :email, :password)", {
+    async insert(): Promise<string> {
+        await pool.execute("INSERT INTO `users`(`id`, `name`, `email`, `password`) VALUES (:id, :name, :email, :password)", {
             id: this.id,
             name: this.name,
             email: this.email,
             password: this.password,
-        })
+        });
+        return this.id;
     }
 
 
