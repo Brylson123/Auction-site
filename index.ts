72f9717c4a5ea9592b33f44bf2ac7403cdd8ddcb
bryla <csowscy@gmail.com>
2023-06-01 23:24:05 +0200

    add some routers

--- index.ts
+++ index.ts
@@ -1,17 +1,19 @@
 import * as express from 'express';
 import * as methodOverride from 'method-override';
-import { urlencoded } from 'express';
+import {urlencoded} from 'express';
+import {UserRecord} from "./records/user.record";
+import {userProfileRouter} from "./routers/userProfile";
+import {homeRouter} from "./routers/home";
 import './utils/db';
 
 const app = express();
 
 app.use(methodOverride('_method'));
 app.use(urlencoded({
-  extended: true,
+    extended: true,
 }));
 
-app.get('/', (req, res) => {
-  res.send('It works!');
-});
+app.use('/', homeRouter);
+app.use('/user', userProfileRouter);
 
 app.listen(3000, 'localhost', () => console.log('listening on http://localhost:3000'));
---
+++ routers/userProfile.ts
@@ -0,0 +1,8 @@
+import {Router} from "express";
+
+export const userProfileRouter = Router();
+
+userProfileRouter
+    .get("/profile/:id", async (req, res) => {
+        res.send(`Welcome to user profile ${req.params.id}`);
+    })
