f15f1f450732d78d03c5841f860a6f042c2b0254
bryla <csowscy@gmail.com>
2023-06-15 00:49:58 +0200

    add login and validation

--- records/user.record.ts
+++ records/user.record.ts
@@ -2,6 +2,7 @@ import {v4 as uuid} from "uuid";
 import {pool} from "../utils/db";
 import {FieldPacket} from "mysql2";
 import {ValidationError} from "../utils/error";
+import {emailRegex} from "../utils/emailRegex";
 
 type UserRecordResults = [UserRecord[], FieldPacket[]]
 
@@ -11,15 +12,17 @@ export class UserRecord {
     public email: string;
     public password: string;
 
-    constructor(obj: Omit<UserRecord, 'insert' | 'update'>) {
+    constructor(obj: Omit<UserRecord, 'insert' | 'update' | 'add'>) {
         const {id, name, email, password} = obj;
 
-        if (name.length < 3 || name.length > 50) {
-            throw new ValidationError(`Imię musi posiadać od 3 do 50 znaków. Aktualnie jest to ${name.length}.`)
-        }
-
-        if (password.length < 5 || password.length > 50) {
+        if (password.length < 5 || password.length > 60) {
             throw new ValidationError(`Hasło musi posiadać od 5 do 50 znaków. Aktualnie jest to ${password.length}.`)
+        }
+        if (email.length < 5 || email.length > 50) {
+            throw new ValidationError(`Email musi posiadać od 5 do 50 znaków. Aktualnie jest to ${email.length}.`)
+        }
+        if (!email.match(emailRegex)) {
+            throw new ValidationError('Zły format email');
         }
 
         this.id = id ?? uuid();
@@ -29,7 +32,7 @@ export class UserRecord {
 
     };
 
-    async insert(): Promise<string> {
+    async add(): Promise<string> {
         await pool.execute("INSERT INTO `users`(`id`, `name`, `email`, `password`) VALUES (:id, :name, :email, :password)", {
             id: this.id,
             name: this.name,
@@ -55,4 +58,20 @@ export class UserRecord {
 
         return results.length === 0 ? null : new UserRecord(results[0]);
     };
+
+    static async login(email: string): Promise<UserRecord | null> {
+        const [results] = await pool.execute("SELECT * FROM `users` WHERE `email` = :email", {
+            email,
+        }) as UserRecordResults;
+
+        return results.length === 0 ? null : results[0];
+    }
+
+    static async checkEmail(email: string): Promise<boolean> {
+        const [results] = await pool.execute("SELECT * FROM `users` WHERE `email` = :email", {
+            email,
+        }) as UserRecordResults;
+
+        return results.length > 0;
+    }
 }
