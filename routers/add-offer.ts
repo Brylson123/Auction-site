439ca382d5418efb5c6aa728d52ad7d1447a0325
bryla <csowscy@gmail.com>
2023-06-15 00:52:09 +0200

    correction

--- routers/add-offer.ts
+++ routers/add-offer.ts
@@ -5,18 +5,19 @@ import {OfferRecord} from "../records/offer.record";
 export const offerRouter = Router();
 
 offerRouter
-    .get('/add-form',(req, res) => {
-    res.render('offer/add-form')
-})
+    .get('/add-form', (req, res) => {
+        res.render('offer/add-form')
+    })
+
     .post('/', async (req, res) => {
-        const {nameOfProduct, price, amount, conditon, description} = req.body;
+        const {nameOfProduct, price, amount, condition, description} = req.body;
 
         const offer = new OfferRecord({
             ...req.body,
             nameOfProduct: String(nameOfProduct),
             price: Number(price),
             amount: Number(amount),
-            conditon: String(conditon),
+            condition: String(condition),
             description: String(description),
         });
 
